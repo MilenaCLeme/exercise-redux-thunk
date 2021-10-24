9cde0a9c43e7d7664c9be9530089a7ef60cffff9
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-24 15:31:50 -0300

    Finalizou o exercicio

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,12 @@
 export const REQUEST_API = 'REQUEST_API';
 export const GET_PICTURE = 'GET_PICTURE';
 
-export const requestAPI = () => ({ type: REQUEST_API });
-
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
-  return (dispatch) => {
-    dispatch(requestAPI());
-    return fetch('https://aws.random.cat/meow')
-      .then((r) => r.json()
-        .then(
-          (json) => dispatch(getPicture(json))
-        ));
+  return async (dispatch) => {
+    const api = await fetch('https://aws.random.cat/meow')
+    const resultado = await api.json();
+    return dispatch(getPicture(resultado));  
   };
 }
